ff1114ac39226a859156bceff966e711799a8d2d
ArnoldAbraham <arnold.abraham@avl.com>
2019-10-10 10:23:48 +0200

    Initial commit

---
+++ demo/module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { DragAndDropModule } from 'angular-draggable-droppable';
+import { DemoUtilsModule } from '../demo-utils/module';
+import { DemoComponent } from './component';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    DragAndDropModule,
+    DemoUtilsModule
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent]
+})
+export class DemoModule {}
